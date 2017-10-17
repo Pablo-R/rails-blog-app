4559b7e2b4fbbb905a7c13556115effdb1bb60d0
Pablo-R <paabloo100@gmail.com>
2017-10-17 22:29:49 +0100

    Define Post controller, create and save post to db from form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+	def new
+		@post = Post.new
+		render('new_post_form')
+	end
+
+	def create
+		@post = Post.new(post_params_permitted)
+		@post.save ? redirect_to(@post) : render('new_post_form')
+	end
+
+	def post_params_permitted
+		params.require(:post).permit(:title, :content, :datetime)
+	end
+end
