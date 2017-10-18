8a847e5a23c8bad9c947fe8d1ccb4c3c5e290d2e
Pablo-R <paabloo100@gmail.com>
2017-10-18 07:10:45 +0100

    Redirect to specific pages on sign_in and sign_out and add name and nick columns to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018054911) do
+ActiveRecord::Schema.define(version: 20171018070010) do
 
   create_table "comments", force: :cascade do |t|
     t.string "nick"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20171018054911) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "nick"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
