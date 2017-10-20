3604d93e3e1408176714e99e6daf275ec6f7d8fd
Pablo-R <paabloo100@gmail.com>
2017-10-20 14:32:10 +0100

    Define relationship many-to-many in Post model with PostsTag and Tag model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
+  has_many :posts_tags
+  has_many :tags, through: :posts_tags
 	has_many :comments, dependent: :destroy
 	
 	validates :title, presence: true, length: { minimum: 5 }
